eebb2095b939aabbc77e6130d00297e9109702e0
nedsko <nedoskobalj94@gmail.com>
2024-08-26 20:39:28 +0200

    initial commit

---
+++ src/main/java/com/example/tagtrace/TagtraceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tagtrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagtraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagtraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tagtrace/TagtraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tagtrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagtraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
